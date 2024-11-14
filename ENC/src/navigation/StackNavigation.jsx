fb19088ed69e163d736032c463fc5a735f988432
yslmoment <164312366+yslmoment@users.noreply.github.com>
2024-11-15 00:11:10 +0900

    ✨feat: MainScreen 수정 및 NoticeScreen 구현

--- ENC/src/navigation/StackNavigation.jsx
+++ ENC/src/navigation/StackNavigation.jsx
@@ -5,6 +5,7 @@ import HomeScreen from "../pages/HomeScreen/HomeScreen";
 import DetailsScreen from "../pages/DetailsScreen/DetailsScreen";
 import StoreScreen from "../pages/StoreScreen/StoreScreen";
 import ReportDangerZoneScreen from "../pages/ReportDangerZoneScreen/ReportDangerZoneScreen";
+import NoticeScreen from "../pages/NoticeScreen/NoticeScreen";
 
 const Stack = createStackNavigator();
 
@@ -32,6 +33,7 @@ const StackNavigation = () => {
         <Stack.Screen name="home" component={HomeScreen} />
         <Stack.Screen name="details" component={DetailsScreen} />
         <Stack.Screen name="store" component={StoreScreen} />
+        <Stack.Screen name="notice" component={NoticeScreen} />
         <Stack.Screen
           name="ReportDangerZone"
           component={ReportDangerZoneScreen}
--- ENC/src/pages/HomeScreen/HomeScreen.jsx
+++ ENC/src/pages/HomeScreen/HomeScreen.jsx
@@ -3,6 +3,7 @@ import {
   View,
   TouchableOpacity,
   Image,
+  ImageBackground,
   StyleSheet,
   Dimensions,
 } from "react-native";
@@ -11,57 +12,67 @@ const { width, height } = Dimensions.get("window");
 
 const HomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("store")}
-      >
-        <Image
-          source={require("./assets/info.png")} // 이미지 파일 경로
-          style={styles.info}
-        />
-      </TouchableOpacity>
+    <ImageBackground
+      source={require("./assets/background.png")} // 배경 이미지 파일 경로
+      style={styles.container} // container 스타일을 ImageBackground에 적용
+    >
+      <Image source={require("./assets/info.png")} style={styles.info} />
       <View style={styles.dashboard}>
         <TouchableOpacity
           style={styles.button}
           onPress={() => navigation.navigate("details")}
         >
           <Image
-            source={require("./assets/dashboard.png")} // 이미지 파일 경로
+            source={require("./assets/dash-board.png")} // 이미지 파일 경로
             style={styles.dashboard}
           />
         </TouchableOpacity>
       </View>
-      <View style={styles.buttonContainer}>
+      <Image source={require("./assets/text.png")} style={styles.text} />
+      <View style={styles.buttoncontainer}>
+        <View style={styles.leftContainer}>
+          <TouchableOpacity
+            style={styles.smallButton}
+            onPress={() => navigation.navigate("store")}
+          >
+            <Image
+              source={require("./assets/store-icon.png")}
+              style={styles.buttonImage}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.smallButton}
+            onPress={() => navigation.navigate("notice")}
+          >
+            <Image
+              source={require("./assets/notice-icon.png")}
+              style={styles.buttonImage}
+            />
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.smallButton}
+            onPress={() => navigation.navigate("ReportDangerZone")}
+          >
+            <Image
+              source={require("./assets/alert.png")}
+              style={styles.buttonImage}
+            />
+          </TouchableOpacity>
+        </View>
+
         <TouchableOpacity
-          style={styles.button}
-          onPress={() => navigation.navigate("store")}
-        >
-          <Image
-            source={require("./assets/store-icon.png")} // 이미지 파일 경로
-            style={styles.image}
-          />
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
+          style={styles.largeButton}
           onPress={() => navigation.navigate("details")}
         >
           <Image
-            source={require("./assets/maps-icon.png")} // 이미지 파일 경로
-            style={styles.image}
+            source={require("./assets/maps-icon.png")}
+            style={styles.buttonImage}
           />
         </TouchableOpacity>
       </View>
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => navigation.navigate("details")}
-      >
-        <Image
-          source={require("./assets/notice-icon.png")} // 추가할 이미지 파일 경로
-          style={styles.noticeImage}
-        />
-      </TouchableOpacity>
-    </View>
+    </ImageBackground>
   );
 };
 
@@ -74,37 +85,63 @@ const styles = StyleSheet.create({
     padding: 10, // 화면 가장자리 여백 추가
   },
   info: {
-    width: width * 0.85, // 화면 너비의 90%
-    height: width * 0.1, // 비율 맞추기 위해 가로 크기와 같은 크기로 설정
+    width: width * 0.9,
+    height: width * 0.9,
     resizeMode: "contain",
-    marginTop: width * 0.1,
-    marginBottom: -30,
+    marginBottom: -120,
+    marginTop: -100,
   },
   dashboard: {
-    width: width * 0.9, // 화면 너비의 90%
-    height: width * 0.9, // 비율 맞추기 위해 가로 크기와 같은 크기로 설정
+    width: width * 1, // 화면 너비의 90%
+    height: width * 1, // 비율 맞추기 위해 가로 크기와 같은 크기로 설정
     resizeMode: "contain",
-    marginBottom: -30,
+    marginBottom: 0,
   },
-  buttonContainer: {
+  text: {
+    width: width * 0.9,
+    height: width * 0.1,
+    resizeMode: "contain",
+    marginBottom: -5,
+    marginTop: 10,
+  },
+  buttoncontainer: {
     flexDirection: "row", // 버튼들을 가로로 정렬
-    justifyContent: "center", // 버튼들 사이에 간격을 균등하게 배치
+    justifyContent: "space-between", // 버튼들 사이에 공간을 균등하게 배분
+    width: "100%", // 화면 너비 전체를 사용
+    height: "30%", // 화면 높이를 꽉 채움
+  },
+
+  leftContainer: {
+    flex: 1, // 화면 왼쪽 절반을 차지
+    justifyContent: "flex-start", // 버튼들을 세로로 상단에 정렬
+    alignItems: "center", // 버튼들 세로로 중앙 정렬
+    paddingVertical: 7, // 버튼들 간의 세로 여백을 추가
+  },
+
+  smallButton: {
+    height: 60, // 버튼 높이를 줄여서 간격을 좁힘
+    width: "100%", // 버튼 너비 설정 (화면 너비에 비례)
+    marginVertical: 3, // 버튼 간의 세로 간격을 3으로 설정하여 좁힘
+    justifyContent: "center",
     alignItems: "center",
-    marginBottom: 20, // 버튼 간 간격 추가
+    borderRadius: 5, // 둥근 모서리
   },
-  button: {
-    marginHorizontal: 0, // 버튼들 사이에 간격을 추가
+
+  largeButton: {
+    flex: 1, // 화면 오른쪽 절반을 차지
+    justifyContent: "center",
+    alignItems: "center",
+    height: 200, // 큰 버튼의 높이를 설정
+    width: "80%", // 버튼 너비 설정 (화면 너비에 비례)
+    margin: 7, // 버튼 간 간격 설정
+    borderRadius: 5, // 둥근 모서리
   },
-  image: {
-    width: width * 0.45, // 이미지 크기 설정 (화면 너비의 40%)
-    height: width * 0.45, // 이미지 크기 설정 (화면 너비의 40%)
-    resizeMode: "contain",
-  },
-  noticeImage: {
-    width: width * 0.85, // 화면 너비의 90%
-    height: width * 0.85, // 비율 맞추기 위해 가로 크기와 같은 크기로 설정
-    resizeMode: "contain", // 이미지 비율 유지
-    marginTop: -50, // 버튼과 이미지 사이의 간격
+
+  buttonImage: {
+    width: "100%", // 이미지가 버튼을 꽉 채우도록 설정
+    height: "100%", // 이미지가 버튼을 꽉 채우도록 설정
+    resizeMode: "contain", // 이미지 비율 유지하면서 크기 조정
+    borderRadius: 5, // 이미지 모서리 둥글게
   },
 });
 
